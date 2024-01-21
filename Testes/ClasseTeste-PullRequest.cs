4bcbd84f1d8dbff3ab805e0fd75c8fb77f8b3f60
Ricardo Costa <exatos@gmail.com>
2024-01-21 11:45:05 -0300

    Create ClasseTeste-PullRequest.cs

---
+++ Testes/ClasseTeste-PullRequest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testes
+{
+   internal class ClasseTeste_PullRequest
+   {
+
+      
+   }
+
+
+    public class  PrimeiroPullRequest
+    {
+      public PrimeiroPullRequest() { }
+    }
+}
